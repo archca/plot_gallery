f12614746e8ab0bde59cd5b080055681eccf5476
Your Name <mirco.brunner@iaw.unibe.ch>
2019-04-23 13:10:47 +0200

    spider diagram

---
+++ spider diagram.R
@@ -0,0 +1,33 @@
+install.packages("devtools")
+devtools::install_github("ricardo-bion/ggradar")
+
+data(msleep, package="ggplot2")
+print(msleep)
+
+
+
+# create a radar chart
+
+# prepare data
+data(msleep, package = "ggplot2")
+library(ggradar)
+library(scales)
+library(dplyr)
+
+plotdata <- msleep %>%
+  filter(name %in% c("Cow", "Dog", "Pig")) %>%
+  select(name, sleep_total, sleep_rem, 
+         sleep_cycle, brainwt, bodywt) %>%
+  rename(group = name) %>%
+  mutate_at(vars(-group),
+            funs(rescale))
+plotdata
+
+# generate radar chart
+ggradar(plotdata, 
+        grid.label.size = 4,
+        axis.label.size = 4, 
+        group.point.size = 5,
+        group.line.width = 1.5,
+        legend.text.size= 10) +
+  labs(title = "Mammals, size, and sleep")
